0701e297e3bf8845dabd06587de416ca69f7cf46
Claudia SG <63687204+sgClaudia98@users.noreply.github.com>
2021-08-23 10:40:01 -0400

    Initial commit

---
+++ src/components/Experience.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experience">
+    <div class="w-100">
+      <h2 class="mb-5">Experience</h2>
+
+      <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div class="resume-content">
+          <h3 class="mb-0">Senior Frontend Developer</h3>
+          <div class="subheading mb-3">Foobar studios</div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="resume-date text-md-right">
+          <span class="text-primary">May 2014 - Present</span>
+        </div>
+      </div>
+
+      <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div class="resume-content">
+          <h3 class="mb-0">Frontend Developer</h3>
+          <div class="subheading mb-3">Foobar studios</div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="resume-date text-md-right">
+          <span class="text-primary">September 2012 - May 2014</span>
+        </div>
+      </div>
+
+      <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+        <div class="resume-content">
+          <h3 class="mb-0">Junior Web Designer</h3>
+          <div class="subheading mb-3">Baz qux media</div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="resume-date text-md-right">
+          <span class="text-primary">June 2011 - December 2012</span>
+        </div>
+      </div>
+
+      <div class="resume-item d-flex flex-column flex-md-row justify-content-between">
+        <div class="resume-content">
+          <h3 class="mb-0">Web Design Intern</h3>
+          <div class="subheading mb-3">Baz qux media</div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="resume-date text-md-right">
+          <span class="text-primary">September 2009 - May 2011</span>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/components/Interests.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="interests">
+    <div class="w-100">
+      <h2 class="mb-5">Interests</h2>
+      <p>Apart from being a frontend developer, I enjoy most of my time being outdoors. In the winter, I like to go ski and sometimes go ice fishing. During the summer, I enjoy mountain biking, running, and fishing.</p>
+      <p
+        class="mb-0"
+      >I like to create websites for fun, trying out new technologies and tools to improve myself as developer and stay up to date with current tech. I love watching ted talks and reading development stories on Reddit.</p>
+    </div>
+  </section>
+</template>
